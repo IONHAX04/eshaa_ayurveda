6e5def4a210a589a41def1b35c59d7a1f19f07f0
IONHAX04 <thirukumara04@gmail.com>
2024-10-11 18:24:10 +0530

    About us temp

--- src/components/02-About/About.tsx
+++ src/components/02-About/About.tsx
@@ -1,8 +1,18 @@
+import comingsoon from "../../assets/appointment/comingsoon.png";
 
 export default function About() {
   return (
     <div>
-      
+      <div className="emailContactForm mt-5 w-full md:w-12/12 mx-auto">
+        <div className="emailFormContxt stayTuned w-full md:w-6/12 mx-auto">
+          <h2>Coming Soon</h2>
+          <p>This page is in progress... Stay tuned for updates...</p>
+        </div>
+        <div className="emailFormContxt w-full md:w-6/12 mx-auto imageContainer">
+          <img src={comingsoon}
+            style={{ blockSize: "100vh", objectFit: "cover", position: "fixed" }} />
+        </div>
+      </div>
     </div>
   )
 }
